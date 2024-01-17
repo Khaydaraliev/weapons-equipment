d76a5152ef18cf3ec08faa7e277a747f4a24dbec
Ulugbek <143921993+Khaydaraliev@users.noreply.github.com>
2024-01-18 01:22:59 +0500

    app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import pathlib
 import plotly.express as px
 import platform
 
-#temp = pathlib.PosixPath
-#pathlib.PosixPath = pathlib.WindowsPath
-
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
