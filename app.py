0298bc55284c104dadca68f500a2c1fbd939ac7d
XAYDARALIYEV ULUG'BEK <ulugbekng@gmail.com>
2024-01-18 01:06:03 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+import platform
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#plt = platform.system()
+#if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+
+st.title("Weapons & Equipment")
+
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpg', 'jpeg','gif','svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+
+    model = load_learner('weapons_equipment_model.pkl')
+
+    pred,  pred_id, probs=model.predict(img)
+    st.subheader(f'Bashorat: {pred}')
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    fig=px.bar(x=probs*100 , y=model.dls.vocab)
+    st.plotly_chart(fig)
